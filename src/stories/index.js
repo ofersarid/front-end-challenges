c5b0dd6304f3c92611efb109154f36b93b85121e
ofer sarid <ofersarid@gmail.com>
2018-01-23 12:59:57 +0200

    wip

--- src/stories/index.js
+++ src/stories/index.js
@@ -30,11 +30,42 @@ storiesOf('Profile Image Challenge', module).add('Read Me', () => (
         <div>On hover will animate a tooltip that shows the user’s name:</div>
         <img src="/on-hover-tooltip.png" alt="" />
         <div>(use the already installed&nbsp;
-          <a href="https://www.npmjs.com/package/react-tooltip" target="blank">react-tooltip</a>&nbsp;
+          <a href="https://www.npmjs.com/package/react-tooltip" target="blank" >react-tooltip</a>&nbsp;
           package)
         </div>
       </li>
     </ol>
+  </div>
+)).add('profile image comp', () => (
+  <div>Your component Here</div>
+));
+
+storiesOf('Text Area Challenge', module).add('Read Me', () => (
+  <div>
+    <h1>Text area component Challenge</h1>
+    <ol>
+      <li>
+        this component has a min and max number of rows. If input exceeds the max amount of rows a scrollbar will
+        appear.
+      </li>
+      <li>
+        if the user has not yet reached the max number of rows an empty line will always be provided at the bottom
+      </li>
+      <li>
+        Line breaks should work when hitting “Enter”, and word breaks to new line
+      </li>
+      <li>
+        Pasting text in should also preserve the defined behaviour
+      </li>
+      <li>
+        The animation for the textarea height is essential.
+      </li>
+      <li>
+        Please notice that there should be no text “jumps” and that is what makes this a senior challenge (to achieve
+        this you will need to think outside of the box - this is not a naive textarea solution)
+      </li>
+    </ol>
+    <iframe src="https://drive.google.com/file/d/0B44JvAgVgFegSjVHbG1CZHBxNm8/preview" width="640" height="480"></iframe>
   </div>
 )).add('profile image comp', () => (
   <div>Your component Here</div>
