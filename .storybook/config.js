8af15490451074b36e5d0acf74678683b4046981
ofer sarid <ofersarid@gmail.com>
2018-01-24 11:17:22 +0200

    wip

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,20 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 import './style.scss';
 
 function loadStories() {
   require('../src/stories');
 }
+
+setOptions({
+  name: 'Front End Challenges',
+  // url: 'https://github.com/kadirahq/storybook-addon-options',
+  // goFullScreen: false,
+  // showLeftPanel: true,
+  showDownPanel: false,
+  // showSearchBox: true,
+  // downPanelInRight: true,
+  // sortStoriesByKind: false,
+});
 
 configure(loadStories, module);
